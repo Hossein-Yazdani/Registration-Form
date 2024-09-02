e2cdce197d75ad7193a922429d5e5affd21ada5b
unknown <yazdanih80.ce@gmail.com>
2024-09-02 15:41:59 +0330

    adding name and last name input

---
+++ Js/app.js
@@ -0,0 +1,7 @@
+let cityObject = {
+    Iran : ["Mashhad","Nishabor","Golbahar"],
+    Iraq : ["karbala","Najaph","Samera"],
+    Usa : ["New york","Chicago","San Francisco"],
+    France : ["Paris","Nice","Lyon"],
+}
+console.log(cityObject)

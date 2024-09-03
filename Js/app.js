8f9c58e45d9df5279b3a05529651a1e7785f02b1
unknown <yazdanih80.ce@gmail.com>
2024-09-03 23:37:04 +0330

    new design for mini project (login form)

--- Js/app.js
+++ Js/app.js
@@ -1,7 +1,43 @@
-let cityObject = {
-    Iran : ["Mashhad","Nishabor","Golbahar"],
-    Iraq : ["karbala","Najaph","Samera"],
-    Usa : ["New york","Chicago","San Francisco"],
-    France : ["Paris","Nice","Lyon"],
-}
-console.log(cityObject)
+// firstName input
+let inputFirst = document.querySelector(".firstNameInput")
+let firstLable = document.querySelector(".firstNameLable")
+let isSelected = true
+inputFirst.addEventListener('click' , function(){
+    if(isSelected){
+        firstLable.style.display = 'none'
+        isSelected = false
+    }
+})
+inputFirst.addEventListener('blur' , function(){
+    if(!isSelected){
+        firstLable.style.display = 'block'
+        isSelected = true
+    }
+})
+//lastName input
+let lastNameInput = document.querySelector(".lastNameInput")
+let lastNameLable = document.querySelector(".lastNameLable")
+let isLastSelected = true
+lastNameInput.addEventListener('click' , function(){
+    if(isLastSelected){
+        lastNameLable.style.display = 'none'
+        isLastSelected = false
+    }
+})
+lastNameInput.addEventListener('blur' , function(){
+    if(!isLastSelected){
+        lastNameLable.style.display = 'block'
+        isLastSelected = true
+    }
+})
+//modal error
+let modalElem = document.querySelector(".modalErrorInput")
+let submitBtn = document.querySelector(".btn")
+submitBtn.addEventListener('click' , function(){
+    if(inputFirst.value.length < 8 || lastNameInput.value.length < 8){
+        modalElem.style.display = 'block'
+    }else{
+        modalElem.style.display = 'none'
+    }
+})
+
